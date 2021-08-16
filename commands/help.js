6bde2c598255885091ef6e3a675a332b4319a42f
GhostDev <77684468+GhostDev-Discord-Bot-Coder@users.noreply.github.com>
2021-08-16 15:21:59 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,8 +5,10 @@ module.exports = {
     aliases: ["h", "cmd", "command"],
     run: async (client, message, args) => {
         const embed = new Discord.MessageEmbed()
+            .setColor('RANDOM')
             .setTitle("Commands")
             .setDescription(client.commands.map(cmd => `\`${cmd.name}\``).join(", "))
+            .setFooter('Made By GhostDev!')
             .setTimestamp()
         message.channel.send(embed)
     }
